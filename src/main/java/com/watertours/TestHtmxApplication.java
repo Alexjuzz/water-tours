3d6580cbb5c1ce1423cd7adf2995f16fe980cf8f
Alexjuzz <juzzle@mail.ru>
2025-03-05 12:53:33 +0300

    start

--- src/main/java/com/watertours/TestHtmxApplication.java
+++ src/main/java/com/watertours/TestHtmxApplication.java
@@ -1,4 +1,4 @@
-package com.testHtmx;
+package com.watertours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/watertours/TestHtmxApplicationTests.java
+++ src/test/java/com/watertours/TestHtmxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.testHtmx;
+package com.watertours;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
